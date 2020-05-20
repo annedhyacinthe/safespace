4e68ca6e34775ac61b847093865675655b5daba0
Anne Hyacinthe <annedhyacinthe@gmail.com>
2020-05-19 20:24:23 -0400

    rebase

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,36 @@ import Landing from './components/Landing'
 import NavBar from './components/Navbar'
 import Register from './components/Register'
 import NewsFeed from './components/NewsFeed'
+import Communities from './components/Communities'
+import { ThemeProvider } from '@gympass/yoga'
 
 const App = () => {
   return (
     <Router>
-      <UserProvider>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route path='/register'>
-              <Register />
-            </Route>
-            <Route path='/login'>
-              <Login />
-            </Route>
-            <Route exact path='/'>
-              <Landing />
-            </Route>
-            <Route path='/news'>
-              <NewsFeed />
-            </Route>
-          </Switch>
-        </div>
-      </UserProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route path='/register'>
+                <Register />
+              </Route>
+              <Route path='/login'>
+                <Login />
+              </Route>
+              <Route path='/news'>
+                <NewsFeed />
+              </Route>
+              <Route path='/communities'>
+                <Communities />
+              </Route>
+              <Route path='/'>
+                <Landing />
+              </Route>
+            </Switch>
+          </div>
+        </UserProvider>
+      </ThemeProvider>
     </Router>
   )
 }
